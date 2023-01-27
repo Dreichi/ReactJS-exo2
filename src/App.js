1547d71fc7f511cee573983e082c38333eb33f3c
dreichi <damien.jenger@gmail.com>
2023-01-27 16:27:28 +0100

    Correction

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import './App.css';
 import Count from './Components/Count';
 
-//Ajoutez une fonctionnalité qui permet de saisir une valeur de départ pour le compteur à l'aide d'un champ de formulaire. Utilisez les props pour passer cette valeur de départ au composant, et utilisez cette valeur pour initialiser l'état lorsque le composant est monté.
-
-
-
 function App() {
   return (
     <div className="App">
